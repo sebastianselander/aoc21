5042d242cc0367760c2c4e788bae1a62f2a14f90
sebastianselander <sebastian.selander@gmail.com>
2021-12-01 20:25:12 +0100

    counting modules completed automatically

--- app/Main.hs
+++ app/Main.hs
@@ -2,11 +2,10 @@ module Main where
 
 import Text.Printf
 import System.Environment (getArgs)
+import System.Directory (listDirectory)
 import System.CPUTime (getCPUTime)
 import Day1 qualified as D1
-
-completedDays :: Int
-completedDays = 1
+import Day2 qualified as D2
 
 type Day = String
 type Part = String
@@ -16,7 +15,7 @@ main = do
     args <- getArgs
     putStrLn ""
     case args of
-      ["all"] -> printAllSolutions 1
+      ["all"] -> printAllSolutions
       [day,part] -> printSolution day part
 
 pickSolver :: (String -> String) -> (String -> String) -> Part -> (String -> String)
@@ -27,9 +26,14 @@ pickSolver _ _ _   = error "no such part"
 
 printSolution :: Day -> Part -> IO ()
 printSolution day part = do
-   input <- readFile $ "input/Day" <> day
+   input <- readFile $ "input/Day" <> day <> ".txt"
    case day of
      "1" -> evalNTime day part (pickSolver D1.solve1 D1.solve2 part) input
+     "2" -> evalNTime day part (pickSolver D2.solve1 D2.solve2 part) input
+     -- "3" -> evalNTime day part (pickSolver D3.solve1 D3.solve2 part) input
+     -- "4" -> evalNTime day part (pickSolver D4.solve1 D4.solve2 part) input
+     -- "5" -> evalNTime day part (pickSolver D5.solve1 D5.solve2 part) input
+     -- "6" -> evalNTime day part (pickSolver D6.solve1 D6.solve2 part) input
 
 evalNTime :: Day -> Part -> (String -> String) -> String -> IO ()
 evalNTime day part f inp = do
@@ -41,11 +45,17 @@ evalNTime day part f inp = do
     printf   "Time:     %0.3f sec\n" (diff :: Double)
     putStrLn ""
 
-printAllSolutions :: Int -> IO ()
-printAllSolutions n
-  | n == completedDays + 1 = pure ()
-  | otherwise = do
+printAllSolutions :: IO ()
+printAllSolutions = printAllSolutions' 1
+
+printAllSolutions' :: Int -> IO ()
+printAllSolutions' n = do
+    daysCompleted <-
+        length <$> listDirectory "/home/sebastian/Documents/github/aoc21/src"
+    if (n == daysCompleted + 1) then
+      pure ()
+    else do
       printSolution (show n) "1"
       printSolution (show n) "2"
       putStrLn "-------------------"
-      printAllSolutions (n+1)
+      printAllSolutions' (n+1)
---
+++ src/Day2.hs
@@ -0,0 +1,13 @@
+module Day2
+    ( solve1
+    , solve2
+    ) where
+
+parseInput :: String -> [Int]
+parseInput = (map read) . words
+
+solve1 :: String -> String
+solve1 = undefined
+
+solve2 :: String -> String
+solve2 = undefined
