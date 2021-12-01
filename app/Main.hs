2b947526ffafd81dc0249dfaa32d1e92424f7131
sebastianselander <sebastian.selander@gmail.com>
2021-12-01 14:00:14 +0100

    day1

--- app/Main.hs
+++ app/Main.hs
@@ -3,30 +3,30 @@ module Main where
 import Text.Printf
 import System.Environment (getArgs)
 import System.CPUTime (getCPUTime)
-import Data.Text qualified as T
-import Data.Text
 import Day1 qualified as D1
 
 main :: IO ()
 main = do
-    putStrLn "\nStarting..."
+    putStrLn "\nStarting...\n"
     preTime <- getCPUTime
-    [d,p] <- getArgs
-    input <- readFile $ "input/Day" <> d
-    let day = read d :: Int
-    let part = read p :: Int
-    case day of
-      1 -> print $ pickSolver D1.solve1 D1.solve2 part (T.pack input)
+    [day,part] <- getArgs
+    input <- readFile $ "input/Day" <> day
+    printSolution (read day) (read part) input
     postTime <- getCPUTime
     -- no shame yanked from wiki
     let diff = (fromIntegral (postTime - preTime)) / (10^12)
-    printf "Computation time: %0.3f sec\n" (diff :: Double)
+    printf   "Time:     %0.3f sec\n" (diff :: Double)
+    putStrLn "\nDone."
 
 
-
-
-
-pickSolver :: (Text -> Text) -> (Text -> Text) -> Int -> (Text -> Text)
+pickSolver :: (String -> String) -> (String -> String) -> Int -> (String -> String)
 pickSolver p1 p2 1 = p1
 pickSolver p1 p2 2 = p2
 pickSolver _ _ _   = error "no such part"
+
+printSolution :: Int -> Int -> String -> IO ()
+printSolution day part input = case day of
+    1 -> putStrLn $ "Solution: " ++ pickSolver D1.solve1 D1.solve2 part input
+    -- 2 -> putStrLn $ "Solution: " ++ pickSolver D2.solve1 D2.solve2 part input
+    -- 3 -> putStrLn $ "Solution: " ++ pickSolver D3.solve1 D3.solve2 part input
+    -- 4 -> putStrLn $ "Solution: " ++ pickSolver D4.solve1 D4.solve2 part input
--- src/Day1.hs
+++ src/Day1.hs
@@ -3,10 +3,24 @@ module Day1
     , solve2
     ) where
 
-import Data.Text
 
-solve1 :: Text -> Text
-solve1 _ = "Unsolved"
+parseInput :: String -> [Int]
+parseInput = (map read) . words
 
-solve2 :: Text -> Text
-solve2 _ = "Unsolved"
+solve1 :: String -> String
+solve1 = show . depth1 . parseInput
+
+solve2 :: String -> String
+solve2 = show . (depth2 maxBound) . parseInput
+
+depth1 :: [Int] -> Int
+depth1 (x:y:xs)
+  | x < y     = 1 + depth1 (y:xs)
+  | otherwise =     depth1 (y:xs)
+depth1 _ = 0
+
+depth2 :: Int -> [Int] -> Int
+depth2 n (x:y:z:rest)
+  | n < (x+y+z) = 1 + depth2 (x+y+z) (y:z:rest)
+  | otherwise   =     depth2 (x+y+z) (y:z:rest)
+depth2 _ _      = 0
