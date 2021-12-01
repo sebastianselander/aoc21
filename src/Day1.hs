d1eb08675676c6b655a6d7723e99b8bedf94e8d2
sebastianselander <sebastian.selander@gmail.com>
2021-12-01 20:03:10 +0100

    funner solutions added

--- src/Day1.hs
+++ src/Day1.hs
@@ -3,6 +3,7 @@ module Day1
     , solve2
     ) where
 
+import Data.List (foldl')
 
 parseInput :: String -> [Int]
 parseInput = (map read) . words
@@ -11,16 +12,32 @@ solve1 :: String -> String
 solve1 = show . depth1 . parseInput
 
 solve2 :: String -> String
-solve2 = show . (depth2 maxBound) . parseInput
+solve2 = show . depth2 . parseInput
+
+--
+-- Funner :)
+--
 
 depth1 :: [Int] -> Int
-depth1 (x:y:xs)
-  | x < y     = 1 + depth1 (y:xs)
-  | otherwise =     depth1 (y:xs)
-depth1 _ = 0
+depth1 xs = sum . map (fromEnum . uncurry (>)) $ zip (tail xs) xs
 
-depth2 :: Int -> [Int] -> Int
-depth2 n (x:y:z:rest)
-  | n < (x+y+z) = 1 + depth2 (x+y+z) (y:z:rest)
-  | otherwise   =     depth2 (x+y+z) (y:z:rest)
-depth2 _ _      = 0
+depth2 :: [Int] -> Int
+depth2 xs = depth1 $ map (uncurry3 (+)) $ zip3 (tail (tail xs)) (tail xs) xs
+  where
+      uncurry3 f (x,y,z) = (x `f` y `f` z)
+
+--
+-- Clear and easy solutions
+--
+
+recursive :: [Int] -> Int
+recursive (x:y:xs)
+  | x < y     = 1 + recursive (y:xs)
+  | otherwise =     recursive (y:xs)
+recursive _      = 0
+
+recursive2 :: Int -> [Int] -> Int
+recursive2 n (x:y:z:rest)
+  | n < (x+y+z) = 1 + recursive2 (x+y+z) (y:z:rest)
+  | otherwise   =     recursive2 (x+y+z) (y:z:rest)
+recursive2 _ _      = 0
