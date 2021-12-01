b82480a2c0a6487f1f1e53909bb6eaae6c242762
sebastianselander <sebastian.selander@gmail.com>
2021-12-01 14:41:35 +0100

    all pars can be run

--- app/Main.hs
+++ app/Main.hs
@@ -5,28 +5,47 @@ import System.Environment (getArgs)
 import System.CPUTime (getCPUTime)
 import Day1 qualified as D1
 
+completedDays :: Int
+completedDays = 1
+
 main :: IO ()
 main = do
-    putStrLn "\nStarting...\n"
-    preTime <- getCPUTime
-    [day,part] <- getArgs
-    input <- readFile $ "input/Day" <> day
-    printSolution (read day) (read part) input
-    postTime <- getCPUTime
-    -- no shame yanked from wiki
-    let diff = (fromIntegral (postTime - preTime)) / (10^12)
-    printf   "Time:     %0.3f sec\n" (diff :: Double)
-    putStrLn "\nDone."
+    args <- getArgs
+    putStrLn ""
+    case args of
+      ["all"] -> printAllSolutions 1
+      [day,part] -> printSolution day part
 
 
-pickSolver :: (String -> String) -> (String -> String) -> Int -> (String -> String)
-pickSolver p1 p2 1 = p1
-pickSolver p1 p2 2 = p2
+pickSolver :: (String -> String) -> (String -> String) -> String -> (String -> String)
+pickSolver p1 p2 "1" = p1
+pickSolver p1 p2 "2" = p2
 pickSolver _ _ _   = error "no such part"
 
-printSolution :: Int -> Int -> String -> IO ()
-printSolution day part input = case day of
-    1 -> putStrLn $ "Solution: " ++ pickSolver D1.solve1 D1.solve2 part input
-    -- 2 -> putStrLn $ "Solution: " ++ pickSolver D2.solve1 D2.solve2 part input
-    -- 3 -> putStrLn $ "Solution: " ++ pickSolver D3.solve1 D3.solve2 part input
-    -- 4 -> putStrLn $ "Solution: " ++ pickSolver D4.solve1 D4.solve2 part input
+
+printSolution :: String -> String -> IO ()
+printSolution day part = do
+   input <- readFile $ "input/Day" <> day
+   case day of
+     "1" -> evalNTime day part (pickSolver D1.solve1 D1.solve2 part) input
+
+evalNTime :: String -> String -> (String -> String) -> String -> IO ()
+evalNTime day part f inp = do
+    preTime <- getCPUTime
+    putStrLn $ "Day " <> day <> ", part " <> part
+    putStrLn $ "Solution: " ++ (f inp)
+    postTime <- getCPUTime
+    let diff = (fromIntegral (postTime - preTime)) / (10^12)
+    printf   "Time:     %0.3f sec\n" (diff :: Double)
+    putStrLn ""
+
+printAllSolutions :: Int -> IO ()
+printAllSolutions n
+  | n == completedDays + 1 = pure ()
+  | otherwise = do
+      printSolution (show n) "1"
+      printSolution (show n) "2"
+      putStrLn "-------------------"
+      printAllSolutions (n+1)
+
+
