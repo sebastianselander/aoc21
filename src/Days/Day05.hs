5d239bfd8bbad0986833bf322f6048d0fe69da1a
sebastianselander <sebastian.selander@gmail.com>
2021-12-05 23:53:53 +0100

    meh

--- src/Days/Day05.hs
+++ src/Days/Day05.hs
@@ -5,9 +5,11 @@ module Days.Day05
 
 import Misc
 
+-- kinda cringe nubbing
 solve1 :: String -> Int
 solve1 = length . nub . concat . allOverlap . filter (not . diagonal) . parseInput
 
+-- even more cringe here
 solve2 :: String -> Int
 solve2 = length . nub . concat . allOverlap . parseInput
 
@@ -43,11 +45,12 @@ findOrientation (a1,a2) (b1,b2) (c1,c2) =
       1    -> Clockwise
       _ -> error "can't find orientation"
 
--- useless since it doesn't consider how many overlapping points :((
 overlap :: Line Int -> Line Int -> Bool
 overlap (Line (x1,y1) (x2,y2)) (Line (a1,b1) (a2,b2)) = 
-   (findOrientation (x1,y1) (x2,y2) (a1,b1) /= findOrientation (x1,y1) (x2,y2) (a2,b2) && 
-    findOrientation (a1,b1) (a2,b2) (x1,y1) /= findOrientation (a1,b1) (a2,b2) (x2,y2))`xor`
+   (findOrientation (x1,y1) (x2,y2) (a1,b1) /= 
+    findOrientation (x1,y1) (x2,y2) (a2,b2) && 
+    findOrientation (a1,b1) (a2,b2) (x1,y1) /= 
+    findOrientation (a1,b1) (a2,b2) (x2,y2))`xor`
    (Collinear == findOrientation (x1,y1) (x2,y2) (a1,b1) && 
     Collinear == findOrientation (x1,y1) (x2,y2) (a2,b2) && 
     Collinear == findOrientation (a1,b1) (a2,b2) (x1,y1) && 
@@ -60,12 +63,17 @@ overlap (Line (x1,y1) (x2,y2)) (Line (a1,b1) (a2,b2)) =
 
 getPoints :: Line Int -> [(Int, Int)]
 getPoints l@(Line (x1,y1) (x2,y2)) 
-  | diagonal l = zip [x1, x1 + signum (x2 - x1) .. x2] [y1, y1 + signum (y2 - y1) .. y2]
-  | otherwise  = [(xs,ys) | xs <- [min x1 x2 .. max x1 x2], ys <- [min y1 y2 .. max y1 y2]]
+  | diagonal l = zip [x1, x1 + signum (x2 - x1) .. x2]
+                     [y1, y1 + signum (y2 - y1) .. y2]
+  | otherwise  = [(xs,ys) | xs <- [min x1 x2 .. max x1 x2],
+                            ys <- [min y1 y2 .. max y1 y2]]
 
 getOverlappingPoints :: Line Int -> Line Int -> [((Int,Int), Int)]
 getOverlappingPoints l1 l2
-  | overlap l1 l2 = filter ((>=2) . snd) $ map (\x -> (head x, length x)) $ group $ sort $ getPoints l1 <> getPoints l2
+  | overlap l1 l2 = filter ((>=2) . snd) 
+    . map (\x -> (head x, length x)) 
+    . group 
+    . sort $ getPoints l1 <> getPoints l2
   | otherwise     = []
 
 lineOverlapAll :: Line Int -> [Line Int] -> [((Int, Int ), Int)]
@@ -74,8 +82,4 @@ lineOverlapAll l xs = concatMap (getOverlappingPoints l) xs
 allOverlap :: [Line Int] -> [[((Int, Int), Int)]]
 allOverlap []     = [] 
 allOverlap (x:xs) = lineOverlapAll x xs : allOverlap xs
-
-
-
-
 
