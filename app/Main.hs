23f1909723f174828fc7428d74d10c67e2ecd17a
sebastianselander <sebastian.selander@gmail.com>
2021-12-07 15:00:39 +0100

    im gonna rewrite with avg & median

--- app/Main.hs
+++ app/Main.hs
@@ -39,7 +39,7 @@ type Part = String
 type DPST = ((Day, Part), String, Double)
 
 completedDays :: Int
-completedDays = 6
+completedDays = 7
 
 main :: IO ()
 main = do
--- src/Days/Day07.hs
+++ src/Days/Day07.hs
@@ -6,7 +6,26 @@ module Days.Day07
 import Misc
 
 solve1 :: String -> Int
-solve1 _ = 0
+solve1 str = minimum $ allFuel (fuel id) (interval p) p
+  where
+    p = parseInput str
 
 solve2 :: String -> Int
-solve2 _ = 0
+solve2 str = minimum $ allFuel (fuel num) (interval p) p
+  where
+    p = parseInput str
+
+parseInput :: String -> [Int]
+parseInput = map read . splitOn ','
+
+interval :: [Int] -> [Int]
+interval xs = [minimum xs .. maximum xs]
+
+fuel :: (Int -> Int) -> Int -> [Int] -> Int
+fuel n = sum . map (f . abs . (n -))
+
+allFuel :: (Int -> [Int] -> Int) -> [Int] -> [Int] -> [Int]
+allFuel f xs ys = map (`f` ys) xs
+
+num :: Int -> Int
+num n = n*(n+1) `div` 2
