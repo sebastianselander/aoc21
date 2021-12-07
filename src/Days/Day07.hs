6a2898a5d6c4e5bfe6decc0abd7eccf3150b584c
sebastianselander <sebastian.selander@gmail.com>
2021-12-07 18:05:30 +0100

    KEK

--- src/Days/Day07.hs
+++ src/Days/Day07.hs
@@ -6,24 +6,6 @@ module Days.Day07
 import Misc
 import Control.Arrow ((&&&))
 
--- solve1 :: String -> Int
--- solve1 = ((sum .) =<< map . (abs .) . (-) . median) . parseInput
-
--- solve2 :: String -> Int
--- solve2 = ((sum .) =<< map . ((num . abs) .) . (-) . average) . parseInput
-
--- parseInput :: String -> [Int]
--- parseInput = map read . splitOn ','
-
--- average :: [Int] -> Int
--- average = uncurry div . (sum &&& length) 
-
--- median :: [Int] -> Int
--- median = uncurry (!!) . (sort &&& (flip div 2 . length))
-
--- num :: Int -> Int
--- num = flip div 2 . uncurry (*) . (id &&& (+1))
-
 solve1 :: String -> Int
 solve1 = ((sum .) =<< map . (abs .) . (-) . uncurry (!!) . (sort &&& (flip div 2 . length))) . map read . splitOn ','
 
